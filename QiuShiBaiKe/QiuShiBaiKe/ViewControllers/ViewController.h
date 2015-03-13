8713452562d79cfc9464e506088b6a754ea97ed4
wulei <wulei@wuleideMac.local>
2015-03-13 21:58:02 +0800

    init ui

--- QiuShiBaiKe/QiuShiBaiKe/ViewControllers/ViewController.h
+++ QiuShiBaiKe/QiuShiBaiKe/ViewControllers/ViewController.h
@@ -2,20 +2,12 @@
 //  ViewController.h
 //  QiuShiBaiKe
 //
-//  Created by wulei on 15-3-10.
+//  Created by wulei on 15-3-13.
 //  Copyright (c) 2015年 wulei. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
-#define GetImageURL_Formatter @"http://pic.qiushibaike.com/system/pictures/%@/%@/medium/%@"
-
-#define ImageHeader_Tag 1
-#define LabelName_Tag 2
-#define LabelContent_Tag 3
-#define Image_Tag 4
-
 @interface ViewController : UITableViewController
 
 @end
-
--- QiuShiBaiKe/QiuShiBaiKe/ViewControllers/ViewController.m
+++ QiuShiBaiKe/QiuShiBaiKe/ViewControllers/ViewController.m
@@ -2,17 +2,15 @@
 //  ViewController.m
 //  QiuShiBaiKe
 //
-//  Created by wulei on 15-3-10.
+//  Created by wulei on 15-3-13.
 //  Copyright (c) 2015年 wulei. All rights reserved.
 //
 
 #import "ViewController.h"
-
-#import "QBNetwork.h"
 #import "QBQiuShiItem.h"
+#import "QBNetwork.h"
 #import "QBUserInfo.h"
 #import "QBVoteInfo.h"
-#import "UIImageView+WebCache.h"
 
 @interface ViewController ()
 
@@ -23,9 +21,9 @@
     NSMutableArray* items;
 }
 
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
     
     NSArray* data =[[QBNetwork sharedNetwork] getQiuShiList];
     
@@ -35,9 +33,6 @@
         QBQiuShiItem* item = [[QBQiuShiItem alloc] initWithDictionary: dic];
         [items addObject: item];
     }
-    
-    self.tableView.delegate = self;
-    self.tableView.dataSource = self;
 }
 
 - (void)didReceiveMemoryWarning {
@@ -45,67 +40,144 @@
     // Dispose of any resources that can be recreated.
 }
 
+#pragma mark - Table view data source
 
-- (NSInteger) tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    
+    return 1;
+    
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
     
     return [items count];
     
 }
 
 
-- (UITableViewCell*) tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    //NSString* noImageIdentifier = @"NoImageIdentifier";
-    NSString* imageIdentifier = @"ImageIdentifier";
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    static NSString* identifier = @"CustomCell";
+    UITableViewCell *cell = [self.tableView dequeueReusableCellWithIdentifier:identifier];
+    
+    UILabel* lblUsername = (UILabel*)[cell viewWithTag: 1];
+    UILabel* lblContent =  (UILabel*)[cell viewWithTag: 2];
     
     NSInteger row = indexPath.row;
-    QBQiuShiItem* item = [items objectAtIndex: row];
+    QBQiuShiItem *item = [items objectAtIndex: row];
     
-    /*
-    UITableViewCell* cell = nil;
-    if([item.image  isEqual: @""]) {
-        cell = [self.tableView dequeueReusableCellWithIdentifier: noImageIdentifier];
-    }
-    else {
-        cell = [self.tableView dequeueReusableCellWithIdentifier: imageIdentifier];
-    }
-    
-    UILabel* lblName = (UILabel*)[self.tableView viewWithTag: LabelName_Tag];
-    lblName.text = item.user.loginName;
-    
-    UILabel* lblContent = (UILabel*)[self.tableView viewWithTag: LabelContent_Tag];
+    lblUsername.text = item.user.loginName;
     lblContent.text = item.content;
     
+    NSAttributedString* attrStr_name = [[NSAttributedString alloc] initWithString: lblUsername.text];
+    lblUsername.attributedText = attrStr_name;
+    NSRange range = NSMakeRange(0, attrStr_name.length);
+    NSDictionary* dic = [attrStr_name attributesAtIndex: 0 effectiveRange: &range];
     
-    UIImageView* imageHeader = (UIImageView*)[self.tableView viewWithTag: ImageHeader_Tag];
-    [imageHeader sd_setImageWithURL: [NSURL URLWithString: @""] placeholderImage: [UIImage imageNamed: @""]];
+    // 计算文本的大小
+    CGSize textSize_name = [lblUsername.text boundingRectWithSize:lblUsername.bounds.size // 用于计算文本绘制时占据的矩形块
+                                                    options:NSStringDrawingUsesLineFragmentOrigin | NSStringDrawingUsesFontLeading // 文本绘制时的附加选项
+                                                 attributes:dic        // 文字的属性
+                                                    context:nil].size;
+    
+    NSAttributedString* attrStr_content = [[NSAttributedString alloc] initWithString: lblContent.text];
+    lblContent.attributedText = attrStr_content;
+    NSRange range_content = NSMakeRange(0, attrStr_content.length);
+    NSDictionary* dic_content = [attrStr_content attributesAtIndex: 0 effectiveRange: &range_content];
+    
+    // 计算文本的大小
+    CGSize textSize_content = [lblContent.text boundingRectWithSize:lblContent.bounds.size // 用于计算文本绘制时占据的矩形块
+                                                         options:NSStringDrawingUsesLineFragmentOrigin | NSStringDrawingUsesFontLeading // 文本绘制时的附加选项
+                                                      attributes:dic_content        // 文字的属性
+                                                         context:nil].size;
     
     
-    NSString* id = [item.itemId substringWithRange: NSMakeRange(0, 4)];
-    NSString* urlStr = [NSString stringWithFormat: GetImageURL_Formatter, id, item.itemId, item.image];
-    
-    UIImageView* image = (UIImageView*)[self.tableView viewWithTag: Image_Tag];
-    [image sd_setImageWithURL: [NSURL URLWithString: urlStr] placeholderImage: [UIImage imageNamed: @""]];*/
-    
-    UITableViewCell* cell = [self.tableView dequeueReusableCellWithIdentifier: imageIdentifier];
-    
-    if(cell == nil) {
-        cell = [[UITableViewCell alloc] initWithStyle: UITableViewCellStyleSubtitle reuseIdentifier: imageIdentifier];
-    }
-    
-    UILabel* lblName = (UILabel*)[self.tableView viewWithTag: LabelName_Tag];
-    lblName.text = item.user.loginName;
-    
-    NSString* id = [item.itemId substringWithRange: NSMakeRange(0, 4)];
-    NSString* urlStr = [NSString stringWithFormat: GetImageURL_Formatter, id, item.itemId, item.image];
-    UIImageView* image = (UIImageView*)[self.tableView viewWithTag: Image_Tag];
-    [image sd_setImageWithURL: [NSURL URLWithString: urlStr] placeholderImage: [UIImage imageNamed: @"default.jpg"]];
-    
+    CGRect frame = CGRectMake(cell.frame.origin.x
+                              , cell.frame.origin.y
+                              , cell.frame.size.width
+                              , textSize_name.height + textSize_content.height + 20);
+    cell.frame = frame;
     
     return cell;
 }
 
-- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-    return 1;
+
+- (CGFloat) getStringHeight: (NSString*) string withFont: (UIFont*) font {
+
+    NSAttributedString* attrStr = [[NSAttributedString alloc] initWithString: string];
+    
+    UILabel* lblContent = [[UILabel alloc] init];
+    lblContent.font = font;
+    lblContent.attributedText = attrStr;
+    NSRange range = NSMakeRange(0, attrStr.length);
+    NSDictionary* dic = [attrStr attributesAtIndex: 0 effectiveRange: &range];
+    
+    // 计算文本的大小
+    CGSize textSize = [lblContent.text boundingRectWithSize:lblContent.bounds.size // 用于计算文本绘制时占据的矩形块
+                                                  options:NSStringDrawingUsesLineFragmentOrigin | NSStringDrawingUsesFontLeading // 文本绘制时的附加选项
+                                               attributes:dic        // 文字的属性
+                                                  context:nil].size;
+    
+    return  textSize.height;
+    
 }
+
+
+- (NSIndexPath *)tableView:(UITableView *)tableView willSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+        
+    return nil;
+    
+}
+
+- (CGFloat) tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    UITableViewCell *cell = [self tableView:tableView cellForRowAtIndexPath:indexPath];
+    return cell.frame.size.height;
+}
+
+
+/*
+// Override to support conditional editing of the table view.
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
+    // Return NO if you do not want the specified item to be editable.
+    return YES;
+}
+*/
+
+/*
+// Override to support editing the table view.
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        // Delete the row from the data source
+        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
+        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+    }   
+}
+*/
+
+/*
+// Override to support rearranging the table view.
+- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath {
+}
+*/
+
+/*
+// Override to support conditional rearranging of the table view.
+- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
+    // Return NO if you do not want the item to be re-orderable.
+    return YES;
+}
+*/
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
 
 @end
